9a1e0db6899238599f479624715cad93157cb3bc
hehp123 <dic12hed@student.lu.se>
2017-04-11 00:02:50 +0200

    Make the background span everywere and fixed the toggles on the mobile sized menu

--- script.js
+++ script.js
@@ -36,4 +36,12 @@ app.controller("navController", ["$scope", "$location", "$routeParams",
         };
 
         $scope.isNavCollapsed = true;
+
+        $scope.closeNav = function(){
+            if($scope.isNavCollapsed == false){
+                $scope.isNavCollapsed = true;
+            }
+
+        };
+
     }]);
